95223130728fe31b804fe04bc1187ed9526bb6b3
Tejas Narkhede <tejasnarkhede999@gmail.com>
2024-10-22 23:36:29 +0530

    DSA, Java, Js Oct22

--- DSA/Practice/BinarySearch/Leetcode744.java
+++ DSA/Practice/BinarySearch/Leetcode744.java
@@ -1,5 +1,22 @@
+/* 744. Find Smallest Letter Greater Than Target
+You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+Example -
+Input: letters = ["c","f","j"], target = "a"
+Output: "c"
+Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+
+https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+*/
 public class Leetcode744 {
-    public char nextGreatestLetter(char[] letters, char target) {
+
+    public static void main(String[] args) {
+        char[] letters = {'c','f','j'};
+        char target = 'a';
+        System.out.println(nextGreatestLetter(letters, target)); //c
+    }
+
+    public static char nextGreatestLetter(char[] letters, char target) {
         int start = 0;
         int end = letters.length - 1;
         while (start <= end) {
