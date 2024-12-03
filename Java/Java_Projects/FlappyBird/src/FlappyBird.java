d7265ff03731776f971f3ba9d6a1af6c4d42d841
Tejas Narkhede <tejasnarkhede999@gmail.com>
2024-12-03 22:43:32 +0530

    adding bird

--- Java/Java_Projects/FlappyBird/src/FlappyBird.java
+++ Java/Java_Projects/FlappyBird/src/FlappyBird.java
@@ -14,24 +14,51 @@ public class FlappyBird extends Panel {
     Image topPipeImg;
     Image bottomPipeImg;
 
+    // Bird
+    int birdX = boardWidth / 8;
+    int birdY = boardHeight / 2;
+    int birdWidth = 34;
+    int birdHeight = 24;
+
+    class Bird {
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image img) {
+            this.img = img;
+        }
+    }
+
+    // game logic
+    Bird bird;
+
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         // setBackground(Color.blue);
 
-        // load images
-        backgroundImg = new ImageIcon(getClass().getResource("/flappybirdbg.png")).getImage();
-        birdImg = new ImageIcon(getClass().getResource("/flappybird.png")).getImage();
-        topPipeImg = new ImageIcon(getClass().getResource("/toppipe.png")).getImage();
-        bottomPipeImg = new ImageIcon(getClass().getResource("/bottompipe.png")).getImage();
+        // Load images
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+
+        // System.out.println(backgroundImg);
+
+        bird = new Bird(birdImg);
     }
-    
+
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+        super.paintComponents(g);
         draw(g);
     }
-    
+
     public void draw(Graphics g) {
+        // background
         g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
+        // bird
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
     }
 }
-
